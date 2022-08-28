46c7a99727c30b9893096f5ee796cf1edd5741eb
Potato <potato@potato.dot>
2022-08-28 10:09:43 -0500

    Fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ _, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '')
 	return {
 		plugins: [solidPlugin()],
