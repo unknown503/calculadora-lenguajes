a1a7b4586c9ce16c34dd1989711ec1f109b857ab
Potato <potato@potato.dot>
2022-08-27 22:45:10 -0500

    Fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	return {
+		define: {
+			PORT: env.VITE_PORT
+		},
+		plugins: [solidPlugin()],
+		server: {
+			port: env.PORT,
+			// port: 3000,
+			base: "https://calculadora-lenguajes-production.up.railway.app/"
+		},
+		build: {
+			target: 'esnext',
+		},
+	}
 });
+
